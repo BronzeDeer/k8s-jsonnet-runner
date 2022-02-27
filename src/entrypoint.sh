67c168396c3fca83552bfa2e83df375e1fa1460c
Joel Pepper <pepper@bronze-deer.de>
2022-02-27 11:41:07 +0100

    Add missing space in test for NO_DOCSONNET

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,7 +4,7 @@ echo "import '/runner/vendor/k8s-libsonnet/${K8S_VERSION:-1.21}/main.libsonnet'"
 
 export JSONNET_PATH="${JSONNET_PATH+${JSONNET_PATH}:}/runner/lib"
 
-if [ -z "${NO_DOCSONNET+X}"]; then
+if [ -z "${NO_DOCSONNET+X}" ]; then
   export JSONNET_PATH="${JSONNET_PATH}:/runner/vendor/docsonnet"
 fi
 
